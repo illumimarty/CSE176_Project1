575debbcf4ad878947590142bbf9faaa24ce70f1
Marty N <mdrnodado@gmail.com>
2021-11-09 10:28:27 -0800

    tried making predictions, error on features

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -3,6 +3,9 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 
+pd.options.display.max_columns = None
+pd.options.display.max_rows = None
+
 train_fea1 = pd.read_csv('./datasets/train_fea1.csv')
 train_gnd1 = pd.read_csv('./datasets/train_gnd1.csv')
 test_fea1 = pd.read_csv('./datasets/test_fea1.csv')
@@ -36,14 +39,14 @@ x_digit7 = pd.DataFrame(train_fea1[42276:48126])
 train_x = x_digit4.append(x_digit7)
 
 ## test output if X and y work
-with open('test.txt', 'w') as f:
-    x = train_x
-    y = train_y
-    # for output in x:
-    #     f.write(str(output))
-    f.write(str(x))
-    f.write(str(y))
-    f.close()
+# with open('test.txt', 'w') as f:
+#     x = train_x
+#     y = train_y
+#     # for output in x:
+#     #     f.write(str(output))
+#     # f.write(str(x.columns.tolist()))
+#     # f.write(str(y))
+#     f.close()
 
 # X_train, X_test, y_train, y_test = train_test_split(train_x, train_y, test_size=0.25, random_state=123)
 
@@ -54,8 +57,20 @@ model.fit(train_x, train_y.values.ravel())
 
 # fixed data, now gotta show results and accuracy
 
+x_digit4 = pd.DataFrame(test_fea1[4160:5051])
+x_digit7 = pd.DataFrame(test_fea1[7038:8011])
+test_x = x_digit4.append(x_digit7)
+
+y_digit4 = pd.DataFrame(test_gnd1[test_gnd1['1'] == 5])
+y_digit7 = pd.DataFrame(test_gnd1[test_gnd1['1'] == 8])
 
 
+y_pred = pd.Series(model.predict(test_x))
+y_test = pd.concat([y_digit4, y_digit7])
+
+z = pd.concat([y_test, y_pred], axis=1)
+z.columns = ['Truth', 'Prediction']
+# z.head()
 
 
 # model.predict_proba(train_x)
