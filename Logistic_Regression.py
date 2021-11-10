e2cc22e8c4b767471de43231de9e93d10ce2c8c4
Marty N <mdrnodado@gmail.com>
2021-11-10 11:25:24 -0800

    got to plot misclf'ied images, weird result

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
 from scipy.io import loadmat
 from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
 from sklearn.metrics import confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 
@@ -21,13 +23,26 @@ y_train = np.array(mnist['train_gnd1'])
 x_test = np.array(mnist['test_fea1'])
 y_test = np.array(mnist['test_gnd1'])
 
+## EDIT THESE 2 VARS TO CHANGE DIGIT CLASSES ##
+digit1 = 4
+digit2 = 9
+
+d1 = digit1 + 1
+d2 = digit2 + 1
+
+l1 = d1-1
+l2 = d2-1
+r1 = d1+1
+r2 = d2+1
+
+
 """Obtaining subset of data for digits 4 and 7"""
-digit4range = np.where(np.logical_and(y_train > 4, y_train < 6))    # find indexes for specified digit
+digit4range = np.where(np.logical_and(y_train > l1, y_train < r1))    # find indexes for specified digit
 digit4idx = digit4range[0]                                          # list of indexes
 digit4train = x_train[digit4idx]                                    # extract features based on indexes
 digit4gnd = y_train[digit4idx].flatten()                            # extract ground trurth based on indexes
 
-digit7range = np.where(np.logical_and(y_train > 7, y_train < 9))
+digit7range = np.where(np.logical_and(y_train > l2, y_train < r2))
 digit7idx = digit7range[0]
 digit7train = x_train[digit7idx]
 digit7gnd = y_train[digit7idx].flatten()
@@ -39,35 +54,105 @@ y_train = np.concatenate([digit4gnd, digit7gnd])
 
 """Creating, fitting, and making predictions with the model"""
 ## comment out models to test out
-# model = LogisticRegression(solver='liblinear', random_state=0)
-model = LogisticRegression(solver='sag', max_iter=600)
+# model = LogisticRegression(solver='liblinear', random_state=0, max_iter=1000)
+model = LogisticRegression(solver='sag', max_iter=1000)
 
+print("Fitting model...")
 model.fit(x_train, y_train)
 
 ## Creating the test/validation sets
-digit4range = np.where(np.logical_and(y_test > 4, y_test < 6))
+digit4range = np.where(np.logical_and(y_test > l1, y_test < r1))
 digit4idx = digit4range[0]
 digit4test = x_test[digit4idx]
+digit4gnd = y_test[digit4idx].flatten() 
 
-digit7range = np.where(np.logical_and(y_test > 7, y_test < 9))
+digit7range = np.where(np.logical_and(y_test > l2, y_test < r2))
 digit7idx = digit7range[0]
 digit7test = x_test[digit7idx]
+digit7gnd = y_test[digit7idx].flatten() 
 
 x_test = np.concatenate((digit4test, digit7test))
 
+y_test = np.concatenate((digit4gnd, digit7gnd))
+print("Making predictions...")
+y_pred = model.predict(x_test)
+
 """Showcasing accuracy via confusion matrix"""
 # needs some tinkering
-cm = confusion_matrix(y_train, model.predict(x_train))    
-fig, ax = plt.subplots(figsize=(8, 8))
-ax.imshow(cm)
-ax.grid(False)
-ax.xaxis.set(ticks=(4, 7), ticklabels=('Predicted 4s', 'Predicted 7s'))
-ax.yaxis.set(ticks=(4, 7), ticklabels=('Actual 4s', 'Actual 7s'))
-ax.set_ylim(1.5, -0.5)
-for i in range(2):
-    for j in range(2):
-        ax.text(j, i, cm[i, j], ha='center', va='center', color='red')
+cm = confusion_matrix(y_pred, y_test)
+# print(cm)
+
+# print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+score = model.score(x_test, y_test)
+
+## Start comment
+labels = [digit1, digit2]
+fig, ax = plt.subplots()
+tick_marks = np.arange(len(labels))
+plt.xticks(tick_marks, labels)
+plt.yticks(tick_marks, labels)
+# create heatmap
+sns.heatmap(pd.DataFrame(cm), annot=True, cmap="YlGnBu", fmt='g')
+ax.xaxis.set_label_position("top")
+# plt.title('Confusion matrix', y=1.1)
+plt.ylabel('True')
+plt.xlabel('Predicted')
+all_sample_title = 'Accuracy Score: {0}'.format(score)
+plt.title(all_sample_title, size=15)
 plt.show()
+## End comment
+
+## start comment
+"""Visualize misclassified images"""
+index = 0
+misclassifiedIndexes = []
+
+# with open('y_pred.txt', 'w') as f:
+#     x = y_pred
+#     # print(x[0][1])
+#     for item in x:
+#         f.write(str(item) + "\n") 
+#     f.close()
+
+# with open('y_test.txt', 'w') as f:
+#     x = y_test
+#     # print(x[0][1])
+#     for item in x:
+#         f.write(str(item) + "\n") 
+#     f.close()
+    
+# with open('misclf.txt', 'w') as f:
+#     i = 0
+#     x = list(zip(y_pred, y_test))
+#     # print(x[0][1])
+#     for predict, label in x:
+#         # f.write(str(item) + "\n")
+#         i+=1
+#         if predict != label:
+#             # f.write(str(predict) + "," + str(label)) 
+#             f.write(str(i) + "\n")
+#     f.close()    
+
+for label, predict in list(zip(y_pred, y_test)):
+    index +=1
+    if label != predict: 
+        misclassifiedIndexes.append(index)
+
+# with open('test.txt', 'w') as f:
+#     x = misclassifiedIndexes
+#     # print(x[0][1])
+#     for item in x:
+#         f.write(str(item)) 
+#     f.close()
+
+plt.figure(figsize=(20,4))
+for plotIndex, badIndex in enumerate(misclassifiedIndexes[1:5]):
+    plt.subplot(1, 5, plotIndex + 1)
+    plt.imshow(np.reshape(x_test[badIndex], (10,10)), cmap=plt.cm.gray)
+    # plt.title("Predicted: {}, Actual: {}".format(y_test[badIndex], x_test[badIndex]), fontsize = 15)
+plt.show()
+
+## End comment
 
 
 ## Misc. code:
