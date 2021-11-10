cbfdcd9e22a8b873ae72f8f9d09ba84fe8200d98
Marty N <mdrnodado@gmail.com>
2021-11-09 18:06:19 -0800

    trying to split digits 4 and 7, append to pd DF

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -1,90 +1,65 @@
+from numpy.lib.npyio import load
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from scipy.io import loadmat
 
+mnist = loadmat('MNISTmini.mat')
 
 pd.options.display.max_columns = None
 pd.options.display.max_rows = None
 
-train_fea1 = pd.read_csv('./datasets/train_fea1.csv')
-train_gnd1 = pd.read_csv('./datasets/train_gnd1.csv')
-test_fea1 = pd.read_csv('./datasets/test_fea1.csv')
-test_gnd1 = pd.read_csv('./datasets/test_gnd1.csv')
-
-# Train on digit 4 and 7 class(5 and 8)
-
-# Setup y
-y_digit4 = pd.DataFrame(train_gnd1[train_gnd1['1'] == 5])
-y_digit7 = pd.DataFrame(train_gnd1[train_gnd1['1'] == 8])
-
-train_y = y_digit4.append(y_digit7)
-train_y.drop(train_y.tail(2).index,inplace=True) # drop last n rows
+Xtrain = 'train_fea1'
+# p = mnist[Xtrain][0].flat[38]
+# p = [item.flat[0] for item in mnist[Xtrain][0]]
+x_train = [[row.flat[0] for row in line] for line in mnist[Xtrain]]
 
 
+
+
+# print(p)
+# x_train = [[element for element in upperElement] for upperElement in mnist['train_fea1']]
+numFea = len(x_train[0]) # of features = 100
+x_col = []
+
+for i in range(numFea):
+    pixel = 'p' + str(i)
+    x_col.append(pixel)
+    # print(pixel)
+x_digits4 = pd.DataFrame(x_train[24674:30094], columns = x_col)
+x_digits7 = pd.DataFrame(x_train[42278:48128], columns = x_col)
+# x_train = x_digits4.append(x_digits7)
+
+with open('test.txt', 'w') as f:
+    x = x_train
+    f.write(str(x))
+    f.close()
+# df_train = pd.DataFrame(x_train, columns = x_col)
+# print(df_train.head())
+
+
+
+
+
+
+
+
+
+
+
+
+# print(df_train.loc[21])
+
+# print(df_train)
+
+# y_train = [[element for element in upperElement] for upperElement in mnist['train_gnd1']]
+
+# print(mnist.keys())
+# print(mnist['train_fea1'][1])
+# print(str(item.flat[0] for item in mnist['train_fea1'][0])
+
+# newData = list(zip(con))
 # with open('test.txt', 'w') as f:
-#     x = train_y
-#     # for output in x:
-#     #     f.write(str(output))
+#     x = df_train
 #     f.write(str(x))
 #     f.close()
-
-
-
-# Setup X
-# Digit 1 row: 6741 - 12698
-x_digit4 = pd.DataFrame(train_fea1[24672:30092])
-# Digit 7  row: 42276 - 48126
-x_digit7 = pd.DataFrame(train_fea1[42276:48126])
-# train_x = pd.concat([x_digit4, x_digit7])
-train_x = x_digit4.append(x_digit7)
-
-## test output if X and y work
-# with open('test.txt', 'w') as f:
-#     x = train_x
-#     y = train_y
-#     # for output in x:
-#     #     f.write(str(output))
-#     # f.write(str(x.columns.tolist()))
-#     # f.write(str(y))
-#     f.close()
-
-# X_train, X_test, y_train, y_test = train_test_split(train_x, train_y, test_size=0.25, random_state=123)
-
-# Since we are required to use L2 regularization, both version of the following model work 
-model = LogisticRegression(solver='newton-cg')
-# model = LogisticRegression(solver='sag', max_iter=600)
-model.fit(train_x, train_y.values.ravel())
-
-# fixed data, now gotta show results and accuracy
-
-x_digit4 = pd.DataFrame(test_fea1[4160:5051])
-x_digit7 = pd.DataFrame(test_fea1[7038:8011])
-test_x = x_digit4.append(x_digit7)
-
-y_digit4 = pd.DataFrame(test_gnd1[test_gnd1['1'] == 5])
-y_digit7 = pd.DataFrame(test_gnd1[test_gnd1['1'] == 8])
-
-
-y_pred = pd.Series(model.predict(test_x))
-y_test = pd.concat([y_digit4, y_digit7])
-
-z = pd.concat([y_test, y_pred], axis=1)
-z.columns = ['Truth', 'Prediction']
-# z.head()
-
-
-# model.predict_proba(train_x)
-
-# y_digit4 = test_gnd1[test_gnd1['1'] == 5]
-# y_digit7 = test_gnd1[test_gnd1['1'] == 8]
-# test_y = pd.concat([y_digit4, y_digit7])
-
-# x_digit4 = test_fea1[4160:5051]
-# x_digit7 = test_fea1[7038:8011]
-# test_x = pd.concat([x_digit4, x_digit7])
-
-# print(model.predict_proba(test_x))
-
-
-# y_pred = pd.Series(model.predict(test_x))
-# y_test = y_test.reset_index
