93bf64e95ea057a45d11a38a44dc05a7f9104a5d
zephr4 <andyalvarenga4@gmail.com>
2021-11-11 21:11:54 -0800

    Optimized Tolerance

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -5,8 +5,12 @@ from clean import *
 from scipy.io import loadmat
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split, validation_curve
 import matplotlib.pyplot as plt
+import random 
+
+random.seed(68)
 
 def main(): 
 
@@ -32,43 +36,56 @@ def main():
     digit2gnd = getDigitGnd(d2, y_train)
 
     """Creating training set"""
-    x_train = np.concatenate((digit1train, digit2train))
-    y_train = np.concatenate([digit1gnd, digit2gnd])
+    dataX = np.concatenate((digit1train, digit2train))
+    dataY = np.concatenate([digit1gnd, digit2gnd])
 
-    """Creating, fitting, and making predictions with the model"""
-    ## comment out models to test out
-    # model = LogisticRegression(solver='liblinear', random_state=0, max_iter=1000)
-    # model = LogisticRegression(solver='sag', max_iter=1000)
+    x_train, x_valid, y_train, y_valid = train_test_split(dataX, dataY, train_size=0.5)
 
-    ## Creating the test/validation sets
+    """Prepping the Test set"""
     digit1test = getDigitFea(d1, x_test, y_test)
     digit2test = getDigitFea(d2, x_test, y_test)
     digit1gnd = getDigitGnd(d1, y_test)
     digit2gnd = getDigitGnd(d2, y_test)
 
-    x_test = np.concatenate((digit1test, digit2test))
-    y_test = np.concatenate((digit1gnd, digit2gnd))
+    dataXtest = np.concatenate((digit1test, digit2test))
+    dataYtest = np.concatenate((digit1gnd, digit2gnd))
 
-    """Perform cross-validation on the hyperparameter C"""
-    clist = [round(i,2) for i in np.linspace(1,25,50)]
-    train_acc = []
-    valid_acc = []
-    # print(clist)
-    for i in range(50):
-        print("Model ", i)
-        # comment out models to test out
-        # model = LogisticRegression(solver='liblinear', random_state=0, max_iter=1000, C = clist[i]).fit(x_train[:7000,:], y_train[:7000])
-        model = LogisticRegression(solver='sag', max_iter=1000, C = clist[i]).fit(x_train[:7000,:], y_train[:7000])
-        
-        preds_train = model.predict(x_train[:7000,:])
-        train_acc.append(accuracy_score(y_train[:7000], preds_train))
+    x_dummy, x_test, y_dummy, y_test = train_test_split(dataXtest, dataYtest, test_size=0.99)
 
-        preds_valid = model.predict(x_train[7001:,:])
-        valid_acc.append(accuracy_score(y_train[7001:], preds_valid))
+    """Perform cross-validation on the hyperparameter tol"""
+    print("Cross-validating...")
+    tlist = np.logspace(-12, 4, 25)
 
-    plt.plot(clist, train_acc, label="Train")
-    plt.plot(clist, valid_acc, label="Validation")
-    plt.legend()
+    ## ADD PARAMETERS HERE
+    # The optimal value for the tolerance is seen from 1e-6 to 1e-12
+    model = LogisticRegression(solver="liblinear", max_iter=1000)
+    train_score, test_score = validation_curve(model, x_train, y_train,
+                                            param_name="tol",
+                                            param_range=tlist,
+                                            scoring="accuracy",
+                                            cv=5)
+    # Calculating mean of the training score
+    mean_train_score = np.mean(train_score, axis = 1)
+    
+    # Calculating mean of the testing score
+    mean_test_score = np.mean(test_score, axis = 1)
+
+    # Find the value that gives the best accuracy
+    best_idx = np.argmax(mean_test_score, axis=0)
+    best_Tol = tlist[best_idx]
+    print("Best Tol: ", best_Tol)
+    print("Best Accuracy: ", mean_test_score[best_idx])
+
+    # Plot mean accuracy scores for training and testing scores
+    plt.semilogx(tlist, mean_train_score, label = "Training Score", color = 'b')
+    plt.semilogx(tlist, mean_test_score, label = "Cross Validation Score", color = 'g')
+    
+    # Creating the plot
+    plt.title("Validation Curve with Logistic Regression")
+    plt.xlabel("Tol")
+    plt.ylabel("Accuracy")
+    plt.tight_layout()
+    plt.legend(loc = 'best')
     plt.show()
 
 if __name__== "__main__":
