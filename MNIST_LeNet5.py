87df12d00f4b9f064afda62d706e36b91af22ec4
zephr4 <andyalvarenga4@gmail.com>
2021-12-14 15:11:23 -0800

    Update Architecture

--- MNIST_LeNet5.py
+++ MNIST_LeNet5.py
@@ -3,6 +3,7 @@ from torch import nn
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, random_split
 from Neural_Network import process
+from scipy.io import loadmat
 
 """layer 1: convolution with 8 kernels of size 3x3 
     layer 2: 2x2 sub-sampling 
@@ -13,11 +14,10 @@ from Neural_Network import process
 class LeNet5(nn.Module):
     def __init__(self):
         super(LeNet5,self).__init__()
-        self.fc1 = nn.Linear(120, 84)
-        self.fc2 = nn.Linear(84, 10)
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
-        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
-        self.conv3 = nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5)
+        self.fc1 = nn.Linear(500, 5)
+        self.fc2 = nn.Linear(5, 5)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=20, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=20, out_channels=500, kernel_size=5)
 
         # Max pooling will activate 1the features with the most presence
         # We can, try average pooling -  reflects the average of features (smooths image)
@@ -31,7 +31,6 @@ class LeNet5(nn.Module):
         x = self.pool(x)
         x = self.tanh(self.conv2(x))
         x = self.pool(x)
-        x = self.tanh(self.conv3(x))
         x = x.reshape(x.shape[0], -1)
         x = self.tanh(self.fc1(x))
         x = self.fc2(x)
@@ -50,18 +49,20 @@ def main():
                                            transforms.Normalize((0.5,), (0.5,))])
 
     print("Spliting the dataset...")
+    #LeNet5 = loadmat('datasets/MNIST-LeNet5.mat')
     train_set = datasets.MNIST(root='./data', train=True, download=True, transform=padding)
     test_set = datasets.MNIST(root='./data', train=False, download=True, transform=padding)
     train_set, valid_set = random_split(train_set,[50000,10000])
-    
+    print(train_set])
     # DataLoaders are iterable data objects
     trainloader = DataLoader(train_set, batch_size=128)
     validloader = DataLoader(valid_set, batch_size=128)
 
     print("Initialize the network")
     model = LeNet5()
+    print(train_set[1][1])
     
-    process(version, model, trainloader, validloader, epochs, min_valid_loss)
+    # process(version, model, trainloader, validloader, epochs, min_valid_loss)
     
 if __name__== "__main__":
     main()
